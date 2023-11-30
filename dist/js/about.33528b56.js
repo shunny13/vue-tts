670acd642736e419b2e9b0c821a3ed46a1475fd4
Nutu CAMPAN <nutu.campan@hotmail.com>
2023-11-30 15:23:13 +0100

    adding dist

---
+++ dist/js/about.33528b56.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktext_to_speach"]=self["webpackChunktext_to_speach"]||[]).push([[443],{3564:function(t,e,n){n.r(e),n.d(e,{default:function(){return r}});var s=function(){var t=this;t._self._c;return t._m(0)},u=[function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],a=n(1001),c={},l=(0,a.Z)(c,s,u,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.33528b56.js.map
